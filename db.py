b3532424e0f45b7c73d73fd20bc63d270b049899
mbwlodarczyk <maciejblazejwlodarczyk@gmail.com>
2024-05-19 07:49:05 -0400

    scraperv1

---
+++ db.py
@@ -0,0 +1,283 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey,Boolean,Float,DateTime
+from sqlalchemy.orm import relationship,declarative_base
+from sqlalchemy.orm import sessionmaker
+import logging
+import requests
+import ssl
+import socket
+import whois
+from OpenSSL import crypto
+import datetime
+from sqlalchemy.orm import sessionmaker
+from langdetect import detect_langs
+from bs4 import BeautifulSoup
+import logging
+import tldextract
+
+logging.basicConfig(filename='scraper.log', level=logging.INFO)
+
+Base = declarative_base()
+
+
+
+
+
+
+class Domain(Base):
+    __tablename__ = 'domains'
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime,default=datetime.datetime.now())
+    domain = Column(String)
+    urls = relationship("Url", back_populates="domain")
+    whois_txt = Column(String)
+    language = Column(String)
+    pl_prob = Column(Float)
+    # create base on this {'version': 3, 'serialNumber': '04E1A77F4C1411AE28DA565817ED719E395C', 'notBefore': 'May  9 20:33:16 2024 GMT', 'notAfter': 'Aug  7 20:33:15 2024 GMT', 'subjectAltName': (('DNS', '*.im3q.icu'), ('DNS', 'im3q.icu')), 'OCSP': ('http://e1.o.lencr.org',), 'caIssuers': ('http://e1.i.lencr.org/',), 'issuer_countryName': 'US', 'issuer_organizationName': "Let's Encrypt", 'issuer_commonName': 'E1', 'subject_commonName': 'im3q.icu'}
+    cert_serialNumber = Column(String)
+    cert_version = Column(Integer)
+    cert_signatureAlgorithm = Column(String)
+    cert_subjectAltName = Column(String)
+    cert_OCSP = Column(String)
+    cert_caIssuers = Column(String)
+    cert_issuer_countryName = Column(String)
+    cert_issuer_organizationName = Column(String)
+    cert_issuer_commonName = Column(String)
+    cert_subject_commonName = Column(String)
+    cert_age = Column(Integer)
+
+    # whois info
+    whois_age = Column(Integer)
+    whois_registrar = Column(String)
+    whois_country = Column(String)
+    whois_organization = Column(String)
+
+    urlscan_scanned_1 = Column(Boolean, default=False)
+
+class Url(Base):
+    __tablename__ = 'urls'
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime,default=datetime.datetime.now())
+    url = Column(String)
+    content = Column(String)
+    domain_id = Column(Integer, ForeignKey('domains.id'))
+    domain = relationship("Domain", back_populates="urls")
+    language = Column(String)
+    pl_prob = Column(Float)
+
+# posgres on 192.168.50.172 with yourusername as username and yourpassword as password with db named yourdatabase
+engine = create_engine('postgresql://yourusername:yourpassword@192.168.50.172:5432/yourdatabase')
+# drop the db
+Base.metadata.drop_all(engine)
+try:
+    Base.metadata.create_all(engine)
+except Exception as e:
+    print(e)
+    logging.error(e, exc_info=True)
+    raise
+Session = sessionmaker(bind=engine)
+
+def save_domain(domain):
+    # check if domain is already in the database
+    session = Session()
+    domain_db = session.query(Domain).filter(Domain.domain == domain).first()
+    session.close()
+    if domain_db is not None:
+        return
+    domain_db = Domain(domain=domain)
+    try:
+        details = get_certificate(domain)
+        if details is not None:
+            domain_db.cert_serialNumber = details[1]["serialNumber"]
+            domain_db.cert_version = details[1]["version"]
+            domain_db.cert_signatureAlgorithm = details[1]["signatureAlgorithm"]
+            domain_db.cert_OCSP = details[1]["OCSP"][0]
+            domain_db.cert_caIssuers = details[1]["caIssuers"][0]
+            domain_db.cert_issuer_countryName = details[1]["issuer_countryName"]
+            domain_db.cert_issuer_organizationName = details[1]["issuer_organizationName"]
+            domain_db.cert_issuer_commonName = details[1]["issuer_commonName"]
+            domain_db.cert_subject_commonName = details[1]["subject_commonName"]
+            # calcuate cert age by subtracting notBefore and now datetime
+            now = datetime.datetime.now()
+            notBefore = datetime.datetime.strptime(details[1]["notBefore"], '%b %d %H:%M:%S %Y %Z')
+            domain_db.cert_age = (now - notBefore).days
+
+        else:
+            logging.error("Failed to get certificate for domain: %s" % domain)
+            return
+    except Exception as e:
+        logging.error("Failed to get certificate for domain: %s" % domain)
+        return
+    try:
+        whois_info = get_whois_info(domain)
+        if whois_info is not None:
+            domain_db.whois_txt = whois_info.text
+            domain_db.whois_registrar = whois_info.registrar
+            domain_db.whois_country = whois_info.country
+            domain_db.whois_organization = whois_info.organization
+            # calculate whois age by subtracting creation date and now datetime
+            now = datetime.datetime.now()
+            creation_date = whois_info.creation_date
+            if isinstance(creation_date, list):
+                creation_date = creation_date[0]
+            domain_db.whois_age = (now - creation_date).days
+        else:
+            logging.error("Failed to get whois info for domain: %s" % domain)
+            return
+    except Exception as e:
+        logging.error("Failed to get whois info for domain: %s" % domain)
+        return
+    
+    try:
+        domain_db.language,domain_db.pl_prob = detect_language("https://" + domain)
+    except Exception as e:
+        logging.error("Failed to detect language for domain: %s" % domain)
+        domain_db.language = None
+
+    
+    try:
+        with Session() as session:
+            session.add(domain_db)
+            session.commit()
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        return
+
+def save_url_lang_check(url):
+    # check language and pl probability
+    with Session() as session:
+        url_db = session.query(Url).filter(Url.url == url).first()
+    if url_db is not None:
+        return
+
+    language,pl_prob = detect_language(url)
+    if language is None:
+        return
+    if pl_prob is None:
+        return
+    # if lanuage is pl or prob is higher than 0.5 save url
+    if language == "pl" or pl_prob > 0:
+        save_url(url)
+
+def save_url(url):
+    # check if url is already in the database
+    with Session() as session:
+        url_db = session.query(Url).filter(Url.url == url).first()
+    if url_db is not None:
+        return
+
+    # Get domain from url using libraries
+    extracted = tldextract.extract(url)
+    if extracted.subdomain:
+        domain_with_subdomains = f"{extracted.subdomain}.{extracted.domain}.{extracted.suffix}"
+    else:
+        domain_with_subdomains = f"{extracted.domain}.{extracted.suffix}"
+
+    # Check if domain is already in the database
+    session = Session()
+    domain = session.query(Domain).filter(Domain.domain == domain_with_subdomains).first()
+    session.close()
+    if domain is None:
+        save_domain(domain_with_subdomains)
+        # get domain object
+        session = Session()
+        domain = session.query(Domain).filter(Domain.domain == domain_with_subdomains).first()
+        session.close()
+
+    # get url content and save it using requests
+
+    r = requests.get(url,verify=False)
+    if r.status_code == 200:
+        content = r.text
+    else:
+        content = None
+
+    # get language and pl probability
+    language,pl_prob = detect_language(url)
+    
+    try:
+        url = Url(url=url, content=content, domain=domain,language=language,pl_prob=pl_prob)
+        session = Session()
+        session.add(url)
+        session.commit()
+        session.close()
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        raise
+
+def get_certificate(domain):
+    try:
+        # Establish a secure connection with the domain
+        context = ssl.create_default_context()
+        with socket.create_connection((domain, 443)) as sock:
+            with context.wrap_socket(sock, server_hostname=domain) as ssock:
+                # Retrieve the certificate
+                cert = ssock.getpeercert()
+                
+                # Extract certificate information
+                cert_info = {
+                    "version": cert["version"],
+                    "serialNumber": cert["serialNumber"],
+                    "notBefore": cert["notBefore"],
+                    "notAfter": cert["notAfter"],
+                    "signatureAlgorithm": cert.get("signatureAlgorithm", None),
+                    "subjectAltName": cert.get("subjectAltName", None),
+                    "OCSP": cert.get("OCSP", None),
+                    "caIssuers": cert.get("caIssuers", None),
+                    "issuer_countryName": None,
+                    "issuer_organizationName": None,
+                    "issuer_commonName": None,
+                    "subject_commonName": None
+                }
+                    
+                # Extract issuer fields
+                for field in cert["issuer"]:
+                    field_name, field_value = field[0]
+                    cert_info[f"issuer_{field_name}"] = field_value
+
+                # Extract subject fields
+                for field in cert["subject"]:
+                    field_name, field_value = field[0]
+                    cert_info[f"subject_{field_name}"] = field_value
+                
+                
+                 
+                return None, cert_info
+    except Exception as e:
+        return None, {"error": str(e)}
+    
+def detect_language(url):
+    try:
+        # Fetch the web page content
+        response = requests.get(url,verify=False)
+        response.raise_for_status()  # Raise an exception for non-200 status codes
+        #parse text so that it can be detected and not the html tags using beautify soup
+        #extract text from html
+        soup = BeautifulSoup(response.text, 'html.parser')
+        response = soup.get_text()
+        # Detect the language of the content
+        language_array = detect_langs(response)
+        # get the most probable language
+        language = language_array[0].lang
+        pl_prob = 0
+        for langs in language_array:
+            # if there is pl language in the array return its prob
+            if langs.lang == "pl":
+                pl_prob = langs.prob
+        return language,pl_prob
+    except Exception as e:
+        print(f"Error fetching or detecting language: {e}")
+        return None,None
+
+
+
+
+def get_whois_info(domain):
+    try:
+        # Query WHOIS information for the domain
+        info = whois.whois(domain)
+        
+        # Extract relevant information
+    
+        return info
+    except Exception as e:
+        return {"error": str(e)}
---
+++ scraper.py
@@ -0,0 +1,190 @@
+from db import engine, Domain, Url, save_domain,save_url,Session,save_url_lang_check
+import requests
+import Config
+import logging
+import datetime
+
+#multi-threading
+import threading
+from queue import Queue
+from concurrent.futures import ThreadPoolExecutor
+
+# configure logger to file
+logging.basicConfig(filename='scraper.log', level=logging.INFO)
+
+
+
+
+def get_domains_from_cert_pl():
+    count = 0
+    try:
+        
+        r = requests.get(Config.cert_link, headers=Config.headers, timeout=Config.timeout)
+        if r.status_code == 200:
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                domains = r.json()
+                for domain in domains:
+                    # create now - rescan_time datetime object, convert domain insert date to datetime object and compare
+                    now = datetime.datetime.now() - datetime.timedelta(minutes=Config.rescan_time)
+                    domain['InsertDate'] = datetime.datetime.strptime(domain['InsertDate'], '%Y-%m-%dT%H:%M:%S')
+                    if domain['InsertDate'] > now:
+                        # do it concurently
+                            count += 1
+                            executor.submit(save_domain,domain["DomainAddress"])
+
+                        # wait for all
+                executor.shutdown(wait=True)
+        logging.info("Inserted %s domains" % count)
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        raise
+
+def get_urls_from_domains():
+
+    with Session() as session:
+        # get url count before
+        url_count = session.query(Url).count()
+        with ThreadPoolExecutor(max_workers=10) as executor:
+        # get not scnaned domains
+            domains = session.query(Domain).filter(Domain.urlscan_scanned_1 == False).all()
+
+            for domain in domains:
+                # do it concurently
+                executor.submit(scan_us,domain.domain)
+                domain.urlscan_scanned_1 = True
+                session.commit()
+            # wait for all
+            executor.shutdown(wait=True)
+        # get url count after
+        url_count_after = session.query(Url).count()
+        logging.info("Inserted %s urls" % (url_count_after - url_count))
+
+
+
+def scan_us(domain):
+    try:
+        r = requests.get('https://urlscan.io/api/v1/search/?q=domain:%s' % domain, headers=Config.urlscan_headers, timeout=Config.timeout)
+        if r.status_code == 200:
+            urls = r.json()
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                for url in urls['results']:
+                    # do it concurently
+                    executor.submit(save_url,url["page"]["url"])
+                # wait for all
+                executor.shutdown(wait=True)
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        raise
+
+def get_urls_from_openphish():
+    try:
+        r = requests.get('https://openphish.com/feed.txt', headers=Config.headers, timeout=Config.timeout)
+        if r.status_code == 200:
+            urls = r.text.split('\n')
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                for url in urls:
+                    # do it concurently
+                    executor.submit(save_url_lang_check,url)
+                # wait for all
+                executor.shutdown(wait=True)
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        raise
+
+def get_urls_from_phishhunt():
+    try:
+        r = requests.get('https://phishunt.io/feed.txt', headers=Config.headers, timeout=Config.timeout)
+        if r.status_code == 200:
+            urls = r.text.split('\n')
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                for url in urls:
+                    # do it concurently
+                    executor.submit(save_url_lang_check,url)
+                # wait for all
+                executor.shutdown(wait=True)
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        raise
+
+def get_urls_from_phishtank():
+    try:
+        r = requests.get('https://data.phishtank.com/data/online-valid.json', headers=Config.headers, timeout=Config.timeout)
+        if r.status_code == 200:
+            urls = r.json()
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                for url in urls:
+                    # do it concurently
+                    executor.submit(save_url_lang_check,url)
+                # wait for all
+                executor.shutdown(wait=True)
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        raise
+
+
+def get_urls_from_phishfindr():
+    try:
+        r = requests.get('https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-links-ACTIVE-NOW.txt', headers=Config.headers, timeout=Config.timeout)
+        if r.status_code == 200:
+            urls = r.json()
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                for url in urls:
+                    # do it concurently
+                    executor.submit(save_url_lang_check,url)
+                # wait for all
+                executor.shutdown(wait=True)
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        raise
+
+def get_urls_from_urlscan():
+    try:
+        r = requests.get('https://urlscan.io/api/v1/search/?q=task.tags:%22%23phishing%22', headers=Config.urlscan_headers, timeout=Config.timeout)
+        if r.status_code == 200:
+            urls = r.json()
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                for url in urls['results']:
+                    # do it concurently
+                    executor.submit(save_url,url["page"]["url"])
+                # wait for all
+                executor.shutdown(wait=True)
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        raise
+
+if __name__ == '__main__':
+    logging.info("Starting scraper "+str(datetime.datetime.now()))
+    logging.info("Getting domains from cert.pl")
+    try:
+        get_domains_from_cert_pl()
+        logging.info("Getting urls from domains")
+        get_urls_from_domains()
+    except Exception as e:
+        logging.error(e, exc_info=True)
+    try:
+        logging.info("Getting urls from openphish")
+        get_urls_from_openphish()
+    except Exception as e:
+        logging.error(e, exc_info=True)
+    try:
+        logging.info("Getting urls from phishhunt")
+        get_urls_from_phishhunt()
+    except Exception as e:
+        logging.error(e, exc_info=True)
+    try:
+        logging.info("Getting urls from phishtank")
+        get_urls_from_phishtank()
+    except Exception as e:
+        logging.error(e, exc_info=True)
+    try:
+        logging.info("Getting urls from phishfindr")
+        get_urls_from_phishfindr()
+    except Exception as e:
+        logging.error(e, exc_info=True)
+    try:
+        logging.info("Getting urls from urlscan")
+        get_urls_from_urlscan()
+    except Exception as e:
+        logging.error(e, exc_info=True)
+    logging.info("Ending scraper "+str(datetime.datetime.now()))
+
